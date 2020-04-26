0d55d28d9af696703cd1e32cf7a75d8301c389b3
Hunar Sekhon <hunar_sekhon@sfu.ca>
2020-04-25 18:08:03 -0700

    my blog site

---
+++ src/build/precache-manifest.ce11e29c159008402660166ec34100db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cced54a990a79b99139c7f4d8f2184f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f76be857bb11ecf71a4e",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "318863de53af58c39598",
+    "url": "/static/js/2.125c1aa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.125c1aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76be857bb11ecf71a4e",
+    "url": "/static/js/main.7a85b74e.chunk.js"
+  },
+  {
+    "revision": "62167d2f0ac43345356c",
+    "url": "/static/js/runtime-main.bfbb93fc.js"
+  }
+]);
